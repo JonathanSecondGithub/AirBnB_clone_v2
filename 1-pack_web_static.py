43c29f2c925c5c7349face585429436d3f244e0b
Jonathan <kariuki.jonathan@students.jkuat.ac.ke>
2023-07-10 01:09:06 +0300

    new_files

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,28 +1,19 @@
 #!/usr/bin/python3
-'''module:
-create tarball artifact of static files in local
-'''
-
 from fabric.api import local
-from datetime import datetime
-import os
+from time import strftime
+from datetime import date
 
 
 def do_pack():
-    '''function:
-    generates a .tgz archive from the contents of the web_static folder
-    '''
-    now = datetime.utcnow().strftime('%Y%m%d%H%M%S')
+    """ A script that generates archive the contents of web_static folder"""
 
-    artifact = f'versions/web_static_{now}.tgz'
-    print(f'Packing web_static to {artifact}')
-    if not os.path.exists('versions'):
-        local('mkdir -p versions')
+    filename = strftime("%Y%m%d%H%M%S")
+    try:
+        local("mkdir -p versions")
+        local("tar -czvf versions/web_static_{}.tgz web_static/"
+              .format(filename))
 
-    fab_stat = local(f'tar -cvzf {artifact} web_static')
-    if fab_stat.succeeded:
-        size = os.path.getsize(artifact)
-        print(f'web_static packed: {artifact} -> {size}Bytes')
-        return artifact
-    else:
+        return "versions/web_static_{}.tgz".format(filename)
+
+    except Exception as e:
         return None
